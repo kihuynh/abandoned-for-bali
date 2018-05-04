fa6990bb16f62c0143c1405b255369e3031878f6
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-05-04 12:27:48 -0700

    add show method for product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,7 +19,16 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
+  def show
+    @products = Product.all
+    @product  = Product.find(params[:id])
+    respond_to do |format|
+      format.html { redirect_to products_path }
+      format.js
+    end
+  end
+
+private
   def product_params
     params.require(:product).permit(:name, :description, :price)
   end
