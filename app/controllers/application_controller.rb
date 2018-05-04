de3325c516a636e98caa6f43e12679c6acb5dceb
Kimberly Huynh <kimberly.huynh@gmail.com>
2018-05-04 10:51:27 -0700

    adding an item to cart works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,12 @@ class ApplicationController < ActionController::Base
   end
 
   def current_order
-    session[:order_id] ? Order.find(session[:order_id]) : Order.new
+    # session[:order_id] ? Order.find(session[:order_id]) : Order.new
+    if session[:order_id]
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
   end
 
   def authorize
